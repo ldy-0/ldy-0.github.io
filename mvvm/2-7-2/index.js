b4201451815cc4115d48eb98fecfdc196fa97db6
person-0 <13213891450@163.com>
2018-05-08 09:45:43 +0800

    Site updated: 2018-05-08 09:45:17

---
+++ mvvm/2-7-2/index.js
@@ -0,0 +1,8 @@
+import san from 'san';
+import {router} from 'san-router';
+import root from './root.san';
+
+router.add({rule: '/', Component: root, target: 'body'});
+
+router.start();
+console.log('task8');
